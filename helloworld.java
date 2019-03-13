40d5c40be8357be408050ddc2744fbe20428ac6e
dilpreetsiddhu <40753734+dilpreetsiddhu@users.noreply.github.com>
2019-03-13 14:29:41 +0530

    deleted line containing "Me chaan lay bhari"

--- helloworld.java
+++ helloworld.java
@@ -3,6 +3,5 @@ class helloworld {
      System.out.println("Hello World");
      System.out.println("Kasa kay tumi bhau!");
      System.out.println("Me chaan");
-       System.out.println("Me chaan lay bhari");
  }
 }
