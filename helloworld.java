0fa56a9e906be7aeae156301c5b5bc104951aa24
Dilpreet Siddhu <Dilpreet.Siddhu@betsol.com>
2019-03-13 16:18:25 +0530

    added master string

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class helloworld {
- public static void main(String args[]){
-     System.out.println("Hello World");
+ Public static void main(String args[]){
+     System.out.println("Hello World-master");
      System.out.println("Kasa kay tumi bhau!");
      System.out.println("Laddoo");
  }
